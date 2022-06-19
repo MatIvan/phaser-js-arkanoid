6bca2aff4e89d43a44ecd47ca16ef544c25d91b0
MatIvan <mativ.rufous@yandex.ru>
2022-06-19 10:56:16 +0300

    fix prod script

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,9 +1,9 @@
 const { merge } = require("webpack-merge");
 const path = require("path");
-const base = require("./base");
+const dev = require("./dev");
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = merge(base, {
+module.exports = merge(dev, {
 	mode: "production",
 	output: {
 		filename: "bundle.min.js",
